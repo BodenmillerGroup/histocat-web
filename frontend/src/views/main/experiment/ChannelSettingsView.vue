0545a224bda511ca3612aced71185ee8a1272257
Anton Rau <anton.rau@gmail.com>
2019-07-23 17:33:09 +0200

    WIP: custom channel colorization.

--- frontend/src/views/main/experiment/ChannelSettingsView.vue
+++ frontend/src/views/main/experiment/ChannelSettingsView.vue
@@ -4,14 +4,11 @@
       <v-layout column>
         <v-layout justify-space-between>
           <b>{{ channel.label }}</b>
-          <v-btn-toggle v-model="colorIndex">
-            <v-btn color="red" x-small @click.stop>R</v-btn>
-            <v-btn color="green" x-small @click.stop>G</v-btn>
-            <v-btn color="blue" x-small @click.stop>B</v-btn>
-            <v-btn color="yellow" x-small @click.stop>Y</v-btn>
-            <v-btn color="cyan" x-small @click.stop>C</v-btn>
-            <v-btn color="#FF00FF" x-small @click.stop>M</v-btn>
-          </v-btn-toggle>
+          <input
+            type="color"
+            v-model.lazy="color"
+            @click.stop
+          />
         </v-layout>
         <v-layout justify-space-between>
           <v-range-slider
@@ -46,7 +43,6 @@
   import { experimentModule } from '@/modules/experiment';
   import { IChannel } from '@/modules/experiment/models';
   import { settingsModule } from '@/modules/settings';
-  import { convertColorToIndex, convertIndexToColor } from '@/utils';
   import ChannelHistogramView from '@/views/main/experiment/ChannelHistogramView.vue';
   import { Component, Prop, Vue, Watch } from 'vue-property-decorator';
 
@@ -59,7 +55,7 @@
 
     @Prop(Object) channel!: IChannel;
 
-    colorIndex = this.channel ? convertColorToIndex(this.metalColor) : -1;
+    color = this.channel ? this.metalColor : '#ffffff';
 
     get levels() {
       const settings = this.settingsContext.getters.channelSettings(this.channel.id);
@@ -81,7 +77,7 @@
     setSharedChannelLevels() {
       const metal = this.channel.metal;
       const settings = this.settingsContext.getters.channelSettings(this.channel.id);
-      let levels = settings && settings.levels ?
+      const levels = settings && settings.levels ?
         [settings.levels.min, settings.levels.max] :
         [this.channel.min_intensity, this.channel.max_intensity];
       this.experimentContext.actions.setSharedChannelLevels({ metal: metal, levels: levels });
@@ -90,14 +86,14 @@
     get metalColor() {
       const colorMap = this.settingsContext.getters.metalColorMap;
       const color = colorMap.get(this.channel.metal);
-      return color ? color : '';
+      return color ? color : '#ffffff';
     }
 
-    @Watch('colorIndex')
-    onColorIndexChanged(colorIndex: number) {
+    @Watch('color')
+    onColorIndexChanged(color: string) {
       this.settingsContext.mutations.setMetalColor({
         metal: this.channel.metal,
-        color: convertIndexToColor(colorIndex),
+        color: color,
       });
     }
   }
