9da5fd66e9f43fc0963d852e01237363b9b73027
Anton Rau <anton.rau@gmail.com>
2019-09-12 17:31:12 +0200

    Webpack build optimization and Vuetify loader integration.

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -9,7 +9,6 @@ import "@/registerServiceWorker";
 import router from "@/router";
 import store from "@/store";
 import Vue from "vue";
-// import "vuetify/dist/vuetify.min.css";
 import "@mdi/font/css/materialdesignicons.css"; // Ensure you are using css-loader
 
 Vue.config.productionTip = false;
@@ -17,6 +16,6 @@ Vue.config.productionTip = false;
 new Vue({
   router,
   store,
-  render: h => h(App),
-  vuetify: vuetify
+  vuetify,
+  render: h => h(App)
 }).$mount("#app");
