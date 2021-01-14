446fd7ff6816d59cf7d9771fcafeac7b56b1f794
Anton Rau <anton.rau@gmail.com>
2021-01-14 10:08:38 +0100

    Some updates before doing project restructuring.

--- frontend/jest.config.js
+++ frontend/jest.config.js
@@ -1,6 +1,9 @@
 module.exports = {
   preset: "@vue/cli-plugin-unit-jest/presets/typescript-and-babel",
-  moduleNameMapper: {
-    "^ky$": require.resolve("ky").replace("index.js", "umd.js"),
-  },
+  // moduleNameMapper: {
+  //   "^ky$": require.resolve("ky").replace("index.js", "umd.js"),
+  // },
+  transformIgnorePatterns: [
+    '/node_modules/(?!(ky))'
+  ]
 };
