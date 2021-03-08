be7b2af9b4709f96fd305cf4c7e427dcac023e6a
Anton Rau <anton.rau@gmail.com>
2021-03-08 13:00:42 +0100

    Tiles export as PNG image.

--- frontend/src/views/group/project/image/visualization/tiles/TilesView.vue
+++ frontend/src/views/group/project/image/visualization/tiles/TilesView.vue
@@ -1,20 +1,35 @@
 <template>
-  <v-container fluid class="overflow-y-auto tiles-view">
-    <v-row>
-      <v-col v-for="item in items" :key="item.name" class="d-flex child-flex" :cols="cols">
-        <v-card flat>
-          <v-card-title class="text-subtitle-2 pa-0">{{ item.caption }}</v-card-title>
-          <v-img :src="`${item.url}`" aspect-ratio="1" class="grey lighten-2" eager>
+  <div v-intersect="onIntersect">
+    <v-toolbar dense flat>
+      <v-menu offset-y>
+        <template v-slot:activator="{ on }">
+          <v-btn v-on="on" small elevation="1">
+            <v-icon left small>mdi-download</v-icon>
+            Export
+          </v-btn>
+        </template>
+        <v-list dense>
+          <v-list-item @click="exportImage">
+            <v-list-item-title>Export PNG</v-list-item-title>
+          </v-list-item>
+        </v-list>
+      </v-menu>
+    </v-toolbar>
+    <v-container ref="tilesContainer" fluid>
+      <v-row>
+        <v-col v-for="item in items" :key="item.name" :cols="cols">
+          <span class="text-subtitle-2">{{ item.caption }}</span>
+          <v-img :src="`${item.url}`" aspect-ratio="1" eager>
             <template v-slot:placeholder>
               <v-row class="fill-height ma-0" align="center" justify="center">
                 <v-progress-circular indeterminate color="grey lighten-5" />
               </v-row>
             </template>
           </v-img>
-        </v-card>
-      </v-col>
-    </v-row>
-  </v-container>
+        </v-col>
+      </v-row>
+    </v-container>
+  </div>
 </template>
 
 <script lang="ts">
@@ -22,11 +37,15 @@ import { apiUrl } from "@/env";
 import { projectsModule } from "@/modules/projects";
 import { settingsModule } from "@/modules/settings";
 import { Component, Vue } from "vue-property-decorator";
+import html2canvas from "html2canvas";
+import { saveAs } from "file-saver";
 
 @Component
 export default class TilesView extends Vue {
   readonly projectsContext = projectsModule.context(this.$store);
   readonly settingsContext = settingsModule.context(this.$store);
+
+  isVisible = false;
 
   get channelsSettings() {
     return this.settingsContext.getters.channelsSettings;
@@ -34,7 +53,7 @@ export default class TilesView extends Vue {
 
   get items() {
     const acquisitionId = this.projectsContext.getters.activeAcquisitionId;
-    if (!acquisitionId) {
+    if (!acquisitionId || !this.isVisible) {
       return [];
     }
     return this.projectsContext.getters.selectedChannels.map((channel) => {
@@ -64,11 +83,25 @@ export default class TilesView extends Vue {
     }
     return 4;
   }
+
+  onIntersect(entries, observer, isIntersecting) {
+    this.isVisible = isIntersecting;
+  }
+
+  async exportImage(e) {
+    const canvas = await html2canvas(this.$refs.tilesContainer as HTMLElement, {
+      useCORS: true,
+      backgroundColor: null
+    });
+    canvas.toBlob(
+      (blob) => {
+        if (blob) {
+          saveAs(blob);
+        }
+      },
+      "image/png",
+      1.0
+    );
+  }
 }
 </script>
-
-<style scoped>
-.tiles-view {
-  height: calc(100vh - 84px);
-}
-</style>
